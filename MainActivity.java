2003ffafb73debad51e792568af07213040e43e1
snowball_213 <81451339+snowball3605@users.noreply.github.com>
2023-08-02 00:10:38 +0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.ui;
+
+import android.Manifest;
+import android.app.AppComponentFactory;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity{
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("https://sosoda.com.tw/"); // 替換成您的網站URL
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+}
